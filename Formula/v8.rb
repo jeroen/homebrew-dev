0ff69acfa570ee97b02d470aeec9a9e1e1313c67
Jeroen Ooms <jeroenooms@gmail.com>
2019-12-23 16:15:31 +0100

    test v8dev formula

---
+++ Formula/v8.rb
@@ -0,0 +1,110 @@
+class V8 < Formula
+  desc "Google's JavaScript engine (bleeding edge)"
+  homepage "https://github.com/v8/v8/wiki"
+  url "https://github.com/v8/v8.git"
+
+  depends_on "llvm" => :build if DevelopmentTools.clang_build_version < 1100
+  depends_on "ninja" => :build
+
+  depends_on :xcode => ["10.0", :build] # required by v8
+
+  # Look up the correct resource revisions in the DEP file of the specific releases tag
+  # e.g. for CIPD dependency gn: https://github.com/v8/v8/blob/7.6.303.27/DEPS#L15
+  resource "gn" do
+    url "https://gn.googlesource.com/gn.git"
+  end
+
+  # e.g.: https://github.com/v8/v8/blob/7.6.303.27/DEPS#L60 for the revision of build for v8 7.6.303.27
+  resource "v8/build" do
+    url "https://chromium.googlesource.com/chromium/src/build.git"
+  end
+
+  resource "v8/third_party/zlib" do
+    url "https://chromium.googlesource.com/chromium/src/third_party/zlib.git"
+  end
+
+  resource "v8/base/trace_event/common" do
+    url "https://chromium.googlesource.com/chromium/src/base/trace_event/common.git"
+  end
+
+  resource "v8/third_party/googletest/src" do
+    url "https://chromium.googlesource.com/external/github.com/google/googletest.git"
+  end
+
+  resource "v8/third_party/jinja2" do
+    url "https://chromium.googlesource.com/chromium/src/third_party/jinja2.git"
+  end
+
+  resource "v8/third_party/markupsafe" do
+    url "https://chromium.googlesource.com/chromium/src/third_party/markupsafe.git"
+  end
+
+  def install
+    (buildpath/"build").install resource("v8/build")
+    (buildpath/"third_party/jinja2").install resource("v8/third_party/jinja2")
+    (buildpath/"third_party/markupsafe").install resource("v8/third_party/markupsafe")
+    (buildpath/"third_party/googletest/src").install resource("v8/third_party/googletest/src")
+    (buildpath/"base/trace_event/common").install resource("v8/base/trace_event/common")
+    (buildpath/"third_party/zlib").install resource("v8/third_party/zlib")
+
+    # Build gn from source and add it to the PATH
+    (buildpath/"gn").install resource("gn")
+    cd "gn" do
+      system "python", "build/gen.py"
+      system "ninja", "-C", "out/", "gn"
+    end
+    ENV.prepend_path "PATH", buildpath/"gn/out"
+
+    # Enter the v8 checkout
+    gn_args = {
+      :is_debug                     => false,
+      :is_component_build           => true,
+      :v8_use_external_startup_data => false,
+      :v8_enable_i18n_support       => false,        # enables i18n support with icu
+      :clang_base_path              => "\"/usr/\"", # uses Apples system clang instead of Google's custom one
+      :clang_use_chrome_plugins     => false,       # disable the usage of Google's custom clang plugins
+      :use_custom_libcxx            => false,       # uses system libc++ instead of Google's custom one
+      :treat_warnings_as_errors     => false,
+    }
+
+    # use clang from homebrew llvm formula on <= High Sierra, because the system clang is to old for V8
+    if DevelopmentTools.clang_build_version < 1100
+      ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib # but link against system libc++
+      gn_args[:clang_base_path] = "\"#{Formula["llvm"].prefix}\""
+    end
+
+    # Transform to args string
+    gn_args_string = gn_args.map { |k, v| "#{k}=#{v}" }.join(" ")
+
+    # Build with gn + ninja
+    system "gn", "gen", "--args=#{gn_args_string}", "out.gn"
+    system "ninja", "-j", ENV.make_jobs, "-C", "out.gn", "-v", "d8"
+
+    # Install all the things
+    (libexec/"include").install Dir["include/*"]
+    libexec.install Dir["out.gn/lib*.dylib", "out.gn/d8"]
+    bin.write_exec_script libexec/"d8"
+  end
+
+  test do
+    assert_equal "Hello World!", shell_output("#{bin}/d8 -e 'print(\"Hello World!\");'").chomp
+    t = "#{bin}/d8 -e 'print(new Intl.DateTimeFormat(\"en-US\").format(new Date(\"2012-12-20T03:00:00\")));'"
+    assert_match %r{12/\d{2}/2012}, shell_output(t).chomp
+
+    (testpath/"test.cpp").write <<~'EOS'
+      #include <libplatform/libplatform.h>
+      #include <v8.h>
+      int main(){
+        static std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+        v8::V8::InitializePlatform(platform.get());
+        v8::V8::Initialize();
+        return 0;
+      }
+    EOS
+
+    # link against installed libc++
+    system ENV.cxx, "-std=c++11", "test.cpp",
+      "-I#{libexec}/include",
+      "-L#{libexec}", "-lv8", "-lv8_libplatform"
+  end
+end
